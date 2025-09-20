aa1c355911dbd0ada59e07806ae5be0da62c9d35
abhi <atanwar@academic.rrc.ca>
2025-09-20 17:25:15 -0500

    Redesign storefront styling and layout

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,27 +1,27 @@
 class ProductsController < ApplicationController
   def index
-    # Show all categories for the filter dropdown
     @categories = Category.order(:name)
 
-    # Start with all products
-    @products = Product.all
-
-    # If a search term is provided, filter by name (case-insensitive)
+    @products = Product.includes(:categories)
     if params[:search].present?
       keyword = params[:search].downcase
       @products = @products.where("LOWER(name) LIKE ?", "%#{keyword}%")
     end
-
-    # If a category_id is provided (i.e. not blank), filter by category
     if params[:category_id].present?
-      @products = @products.where(category_id: params[:category_id])
+      @products = @products.joins(:categories).where(categories: { id: params[:category_id] })
     end
-
-    # Finally, order the results by product name
     @products = @products.order(:name)
+
+    @featured_product = @products.first || Product.includes(:categories).first
+    @highlight_categories = @categories.limit(3)
+    @hero_stats = {
+      products: Product.count,
+      categories: Category.count,
+      satisfaction: "4.9 / 5"
+    }
   end
 
   def show
-    @product = Product.find(params[:id])
+    @product = Product.includes(:categories).find(params[:id])
   end
 end
